9b4c9cfac0e2204bfb32c9f9ec8c4346f688adf5
KayZhang52 <zhangchaofan99627@gmail.com>
2024-04-27 12:23:09 +0800

    first commit to create project

---
+++ src/main/java/com/leetcode/kayzhang52/App.java
@@ -0,0 +1,16 @@
+package com.leetcode.kayzhang52;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    public static void sayHelloExcitedly(){
+        System.out.println("Hello!!!!!!");
+    }
+}
